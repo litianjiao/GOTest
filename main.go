9e2447073729d992501921c5a2843ddce4eeceb9
Troy <litianjiaod3@hotmail.com>
2016-12-18 18:36:07 +0800

    study

--- main.go
+++ main.go
@@ -2,10 +2,30 @@ package main
 
 import (
 	"fmt"
-
 )
+
+func binaryseach(iArray[10] int64, key int64, n int64)  (iMid   int64) {
+
+	var iLow int64 = 0
+	var iHigh= n - 1
+	for (iLow <= iHigh) {
+		iMid = (iHigh + iLow) / 2
+		if (iArray[iMid] > key) {
+			iHigh = iMid - 1
+		} else if iArray[iMid] < key {
+			iLow = iMid + 1
+		} else {
+			return iMid
+		}
+
+	}
+return -1
+}
+
+
 func main() {
-	fmt.Printf("hello")
+	a:=[10] int64{1,2,3,4,5,6,7,8,9,10}
+   fmt.Println("the position is ",(binaryseach(a,4,10)+1))
 
 
 }
