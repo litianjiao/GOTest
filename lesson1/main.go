271d79cdea9f40be1c00cbefbde5ac17ec449e17
Troy <litianjiaod3@hotmail.com>
2017-04-13 22:46:53 +0800

    interface

--- lesson1/main.go
+++ lesson1/main.go
@@ -4,20 +4,27 @@ import (
 	"fmt"
 )
 
+//USB接口
 type USB interface {
 	Name() string
 	Connecter
 }
+
+//连接口
 type Connecter interface {
 	Connect()
 }
+
+//手机连接对象
 type PhoneConnecter struct {
 	name string
 }
 
+//-实现手机上USB口方法（根据interface规定他的名字，实现connecter接口下的connect方法）
 func (pc PhoneConnecter) Name() string {
 	return pc.name
 }
+
 func (pc PhoneConnecter) Connect() {
 	fmt.Println("Connect:", pc.name)
 }
@@ -28,7 +35,7 @@ func Disconnect(usb USB) {
 	}
 }
 func main() {
-
+	//定义对象 手机USB口
 	a := PhoneConnecter{"PhoneConnecter"}
 	a.Connect()
 	Disconnect(a)
