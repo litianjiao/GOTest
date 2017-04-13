c901ede85222a8c099ea9c063e7b28dabb21b306
Troy <litianjiaod3@hotmail.com>
2017-04-13 21:23:38 +0800

    interface

---
+++ lesson0/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	//"fmt"
+	serial"github.com/tarm/goserial"
+	"log"
+)
+
+func binaryseach(iArray[10] int64, key int64, n int64)  (iMid   int64) {
+
+	var iLow int64 = 0
+	var iHigh= n - 1
+	for (iLow <= iHigh) {
+		iMid = (iHigh + iLow) / 2
+		if (iArray[iMid] > key) {
+			iHigh = iMid - 1
+		} else if iArray[iMid] < key {
+			iLow = iMid + 1
+		} else {
+			return iMid
+		}
+
+	}
+return -1
+}
+
+
+func main() {
+
+		c := &serial.Config{Name: "COM10", Baud: 115200}
+		s, err := serial.OpenPort(c)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		n, err := s.Write([]byte("test"))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		buf := make([]byte, 128)
+		n, err = s.Read(buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Print("%q", buf[:n])
+	}
