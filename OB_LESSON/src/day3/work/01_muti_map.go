d9152656bd982a3c4809e9c4712e19df4d25efcd
Troy <litianjiaod3@hotmail.com>
2017-07-20 23:06:26 +0800

    day3 part2
    Signed-off-by: Troy <litianjiaod3@hotmail.com>

---
+++ OB_LESSON/src/day3/work/01_muti_map.go
@@ -0,0 +1,15 @@
+package work
+
+import "fmt"
+
+func multi() {
+	for i := 0; i < 9; i++ {
+		for j := 0; j <= i; j++ {
+			fmt.Printf("%d*%d=%d\t", (i + 1), (j + 1), ((i + 1) * (j + 1)))
+		}
+		fmt.Println()
+	}
+}
+func main() {
+	multi()
+}
