5258d73756f11a9909beaeca721da9da5921ef9c
Troy Li <litianjiaod3@hotmail.com>
2017-09-10 13:32:21 +0800

    add hash balance

--- oblsn/src/day7/exp1_balance/balance/hash.go
+++ oblsn/src/day7/exp1_balance/balance/hash.go
@@ -1,3 +1,33 @@
 package balance
 
-type HashBalance 
+import (
+	"fmt"
+	"hash/crc32"
+	"math/rand"
+)
+
+type HashBalance struct {
+	key string
+}
+
+func init() {
+	RegisterBalancer("hash", &HashBalance{})
+}
+
+func (p *HashBalance) DoBalance(insts []*Instance, key ...string) (inst *Instance, err error) {
+	var defKey string = fmt.Sprintf("%d", rand.Int())
+	if len(key) > 0 {
+		defKey = key[0]
+	}
+	lens := len(insts)
+	if lens == 0 {
+		err = fmt.Errorf("No backend instance")
+		return
+	}
+	crcTable := crc32.MakeTable(crc32.IEEE)
+	hashVal := crc32.Checksum([]byte(defKey), crcTable)
+	//hashval:=md5.Sum(key[:])
+	index := int(hashVal) % lens
+	inst = insts[index]
+	return
+}
